2f715772e1f14410f6bd946d0e416d3c1109b335
Aditya Raj Chauhan <102533268+caringcoder@users.noreply.github.com>
2023-03-16 20:05:22 +0530

    Add files via upload

---
+++ database/dbConnection.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+
+const URI = `mongodb+srv://ypathak188:${process.env.SECRET_PASS}@kanafusi.80styik.mongodb.net/AllData?retryWrites=true&w=majority`
+
+const connection = () => {
+    try {
+        mongoose.connect(URI)
+            .then(() => { console.log("Database Connected") })
+            .catch((e) => { console.log(e.message) })
+    } catch (er) {
+        console.log(e.error)
+    }
+}
+
+module.exports = connection;
